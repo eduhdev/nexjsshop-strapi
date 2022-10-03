872c7eab70d1054d8eb86c06362b14c9850f3f3d
eduhdev <eduardocarvalho970@gmail.com>
2022-10-03 15:50:29 -0300

    restore production database setting

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,25 +1,20 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
+        client: "postgres",
+        host: env("DATABASE_HOST"),
+        port: env.int("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
         ssl: {
           rejectUnauthorized: false,
         },
       },
-      options: {
-        ssl: true,
-      },
+      options: { ssl: true },
     },
   },
 });
